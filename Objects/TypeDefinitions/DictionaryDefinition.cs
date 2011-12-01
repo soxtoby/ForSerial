caaf6b23a50cf623bf9d7620201005035b346b72
soxtoby <frizzletop@gmail.com>
2011-12-02 07:25:28 +1000

    Adding DictionaryDefinition.cs, which I guess I missed.

---
+++ Objects/TypeDefinitions/DictionaryDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace json.Objects
+{
+    public class DictionaryDefinition : JsonArrayDefinition
+    {
+        private readonly TypeDefinition keyTypeDef;
+        private readonly TypeDefinition valueTypeDef;
+
+        private DictionaryDefinition(Type dictionaryType)
+            : base(dictionaryType, typeof(KeyValuePair<object, object>))
+        {
+            keyTypeDef = CurrentTypeHandler.GetTypeDefinition(dictionaryType.GetGenericInterfaceType(typeof(IDictionary<,>), 0));
+            valueTypeDef = CurrentTypeHandler.GetTypeDefinition(dictionaryType.GetGenericInterfaceType(typeof(IDictionary<,>), 1));
+        }
+
+        internal static DictionaryDefinition CreateDictionaryDefinition(Type type)
+        {
+            return type.CanBeCastTo(typeof(IDictionary))
+                ? new DictionaryDefinition(type)
+                : null;
+        }
+
+        public override void AddToCollection(object collection, object item)
+        {
+            KeyValuePair<object, object> keyValuePair = (KeyValuePair<object, object>)item;
+
+            object key = keyTypeDef != null
+                ? keyTypeDef.ConvertToCorrectType(keyValuePair.Key)
+                : keyValuePair.Key;
+            object value = valueTypeDef != null
+                ? valueTypeDef.ConvertToCorrectType(keyValuePair.Value)
+                : keyValuePair.Value;
+
+            ((IDictionary)collection).Add(key, value);
+        }
+    }
+}
