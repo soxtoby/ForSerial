ffda882a361613af0bb2795aefe5698cd1d6843b
soxtoby <frizzletop@gmail.com>
2012-01-04 17:48:28 +1000

    Pulled EnumerableDefinition out into its own file.

---
+++ Serializer/Objects/TypeDefinitions/EnumerableDefinition.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace json.Objects
+{
+    public class EnumerableDefinition : SequenceDefinition
+    {
+        private EnumerableDefinition(Type type, Type itemType)
+            : base(type, itemType)
+        {
+        }
+
+        internal static SequenceDefinition CreateEnumerableDefinition(Type type)
+        {
+            return type.CanBeCastTo(typeof(IEnumerable))
+                       ? new EnumerableDefinition(type, GetIEnumerableItemType(type))
+                       : null;
+        }
+
+        private static Type GetIEnumerableItemType(Type type)
+        {
+            return type.GetGenericInterfaceType(typeof(IEnumerable<>)) ?? typeof(object);
+        }
+
+        public override bool IsSerializable
+        {
+            get { return ItemTypeDef.IsSerializable; }
+        }
+
+        public override TypedObjectArray CreateArray()
+        {
+            return new TypedObjectEnumerable(ItemTypeDef);
+        }
+    }
+}
--- Serializer/Objects/TypeDefinitions/SequenceDefinition.cs
+++ Serializer/Objects/TypeDefinitions/SequenceDefinition.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace json.Objects
 {
@@ -27,36 +26,6 @@ namespace json.Objects
             }
 
             return output;
-        }
-    }
-
-    public class EnumerableDefinition : SequenceDefinition
-    {
-        private EnumerableDefinition(Type type, Type itemType)
-            : base(type, itemType)
-        {
-        }
-
-        internal static SequenceDefinition CreateEnumerableDefinition(Type type)
-        {
-            return type.CanBeCastTo(typeof(IEnumerable))
-                       ? new EnumerableDefinition(type, GetIEnumerableItemType(type))
-                       : null;
-        }
-
-        private static Type GetIEnumerableItemType(Type type)
-        {
-            return type.GetGenericInterfaceType(typeof(IEnumerable<>)) ?? typeof(object);
-        }
-
-        public override bool IsSerializable
-        {
-            get { return ItemTypeDef.IsSerializable; }
-        }
-
-        public override TypedObjectArray CreateArray()
-        {
-            return new TypedObjectEnumerable(ItemTypeDef);
         }
     }
 }
